a233cd7320da107fbf5a86b982c9778728b031ff
gauravsingh906 <gauravsingh8xm@gmail.com>
2025-03-17 16:13:02 +0530

    tick icon alignment margin-top-[1rem] shift

--- src/app/components/VideoSection.jsx
+++ src/app/components/VideoSection.jsx
@@ -338,7 +338,7 @@ const VideoSection = () => {
             <h3 className="text-lg font-bold text-gray-800 mb-3 font-['IntegralCF'] relative z-10">Why Nailib Videos?</h3>
             <ul className="space-y-3 relative z-10">
               <li className="flex items-start bg-white/80 p-2 rounded-lg shadow-sm">
-                <div className="flex-shrink-0 h-5 w-5 text-blue-600">
+                <div className="flex-shrink-0 mt-[1rem] h-5 w-5 text-blue-600">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                   </svg>
@@ -346,7 +346,7 @@ const VideoSection = () => {
                 <p className="ml-2 text-sm text-gray-600">Created by experienced IB examiners and educators</p>
               </li>
               <li className="flex items-start bg-white/80 p-2 rounded-lg shadow-sm">
-                <div className="flex-shrink-0 h-5 w-5 text-blue-600">
+                <div className="flex-shrink-0 mt-[1rem] h-5 w-5 text-blue-600">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                   </svg>
@@ -354,7 +354,7 @@ const VideoSection = () => {
                 <p className="ml-2 text-sm text-gray-600">Perfectly aligned with the latest IB curriculum requirements</p>
               </li>
               <li className="flex items-start bg-white/80 p-2 rounded-lg shadow-sm">
-                <div className="flex-shrink-0 h-5 w-5 text-blue-600">
+                <div className="flex-shrink-0 mt-[1rem] h-5 w-5 text-blue-600">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                   </svg>
